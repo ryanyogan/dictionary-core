d744e14501fa453f4928ec4288eb9b5905e7e07d
Ryan Yogan <ryanyogan@gmail.com>
2020-12-29 15:39:50 -0600

    updated mix file

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Dictionary.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      name: "Dictionary",
+      source_url: "https://github.com/ryanyogan/dictionary-core"
     ]
   end
 
@@ -20,5 +24,17 @@ defmodule Dictionary.MixProject do
 
   defp deps do
     []
+  end
+
+  defp description() do
+    "This is the core of the dictionary service which has around 8k words"
+  end
+
+  defp package() do
+    [
+      name: "dictionary",
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/ryanyogan/dictionary-core"}
+    ]
   end
 end
